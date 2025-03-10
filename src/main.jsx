9dd9baaef0e0a240af396249e26dac71e077ce14
Jose Guglielmi <lufortgamernw@gmail.com>
2025-03-10 20:42:35 -0300

    sadad

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { Menu } from "./components/menu.jsx";
-import { Main1 } from "./components/Main1.jsx";
+import { Menu } from "/src/components/menu.jsx";
+import { Main1 } from "/src/components/Main1.jsx";
 
 createRoot(document.getElementById("root")).render(
   <>
