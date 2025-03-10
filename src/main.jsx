adb517942f2e21a6c8b22e504344ca534fb9f6dc
Jose Guglielmi <lufortgamernw@gmail.com>
2025-03-10 20:36:56 -0300

    s

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Menu } from "./components/menu";
-import { Main } from "./components/Main";
+import { Main } from "./components/Main1";
 
 createRoot(document.getElementById("root")).render(
   <>
